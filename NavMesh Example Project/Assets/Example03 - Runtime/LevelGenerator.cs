165f037c6074c76723ead9d975cb4091cff17650
Seokil Kang <codemaru@gmail.com>
2022-08-03 16:37:51 +0900

    유투브 튜토리얼 진행 스크립트

--- NavMesh Example Project/Assets/Example03 - Runtime/LevelGenerator.cs
+++ NavMesh Example Project/Assets/Example03 - Runtime/LevelGenerator.cs
@@ -1,6 +1,10 @@
 using UnityEngine;
+using UnityEngine.AI;
 
-public class LevelGenerator : MonoBehaviour {
+public class LevelGenerator : MonoBehaviour
+{
+
+	public NavMeshSurface surface;
 
 	public int width = 10;
 	public int height = 10;
@@ -13,6 +17,7 @@ public class LevelGenerator : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		GenerateLevel();
+		surface.BuildNavMesh();
 	}
 	
 	// Create a grid based level
---
+++ NavMesh Example Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class PlayerController : MonoBehaviour
+{
+    public Camera cam;
+    public NavMeshAgent agent;
+    public ThirdPersonCharacter character;
+
+    private void Start()
+    {
+        if (!cam) cam = Camera.main;
+        if (!agent) agent = this.GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+
+        if (agent.remainingDistance > agent.stoppingDistance)
+        {
+            character.Move(agent.desiredVelocity, false, false);
+        }
+        else
+        {
+            character.Move(Vector3.zero, false, false);
+        }
+    }
+}
